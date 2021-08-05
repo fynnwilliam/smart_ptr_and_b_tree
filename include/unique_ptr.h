f11972c8bbaed756a2557a6e5b98c0d4480c4f13
fynnwilliam <fynnwilliam@gmail.com>
2021-08-05 12:13:51 -0400

    updated unique_ptr

--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,13 +1,21 @@
 template<typename T>
-
 class unique_ptr
 {
 private:
     T* ptr;
 public:
+    unique_ptr() : ptr{nullptr} {}
     unique_ptr(T* p) : ptr{p} {}
+    unique_ptr(unique_ptr<T> const&) = delete;
+
+    unique_ptr<T>& operator =(unique_ptr<T> const&) = delete;
     T& operator *() { return *ptr; }
     T* operator ->() { return ptr; }
-    T& operator [](int index) { return ptr[index]; }
-    ~unique_ptr() { delete ptr; ptr = nullptr; }
+    T& operator [](int const& index) { return ptr[index]; }
+
+    ~unique_ptr()
+    {
+        delete ptr;
+        ptr = nullptr;
+    }
 };
