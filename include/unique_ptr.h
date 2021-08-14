dd40e9cee2da34664ad46d4ab2d2534d2f6b8276
fynnwilliam <fynnwilliam@gmail.com>
2021-08-14 11:41:25 -0400

    overloaded operator[]

--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -3,15 +3,18 @@ class unique_ptr
 {
 private:
     T* ptr;
+    
+    unique_ptr(unique_ptr<T> const&) = delete;
+    unique_ptr<T>& operator =(unique_ptr<T> const&) = delete;
+    
 public:
     unique_ptr() : ptr{nullptr} {}
     unique_ptr(T* p) : ptr{p} {}
-    unique_ptr(unique_ptr<T> const&) = delete;
 
-    unique_ptr<T>& operator =(unique_ptr<T> const&) = delete;
     T& operator *() { return *ptr; }
     T* operator ->() { return ptr; }
     T& operator [](int const& index) { return ptr[index]; }
+    T operator [](int const& index) const { return ptr[index]; }
 
     ~unique_ptr()
     {
