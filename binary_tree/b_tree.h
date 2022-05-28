4b2f1a7a488082e0c04c36732f805ff57fa650a2
fynnwilliam <fynnwilliam@gmail.com>
2022-05-28 10:16:38 -0400

    update b_tree.h

--- binary_tree/b_tree.h
+++ binary_tree/b_tree.h
@@ -1,105 +1,65 @@
-class node
-{
-public:    
-    int key{};
-    int height{};
-    node* left{nullptr};
-    node* right{nullptr};
-    node* parent{nullptr};
-    
-    node() = default;
-    explicit node(int d) : key{d} {}
-    node(node const& n) : key{n.key}, height{n.height}, left{n.left}, right{n.right}, parent{n.parent} {}
-    node(node&& n) noexcept : key{n.key}, height{n.height}, left{n.left}, right{n.right}, parent{n.parent}
-    {
-        n.key = 0;
-        n.height = 0;
-        n.left = n.right = n.parent = nullptr;
-    }
-    
-    node& operator =(node n)
-    {
-        _swap(*this, n);
-        
-        return *this;
-    }
-    
-    bool operator>(node const& n) const { return key > n.key; }
-    
-    friend void _swap(node& a, node& b)
-    {
-        std::swap(a.key, b.key);
-        std::swap(a.left, b.left);
-        std::swap(a.right, b.right);
-        std::swap(a.height, b.height);
-        std::swap(a.parent, b.parent);
-    }
-        
-    ~node() {}
-};
-    
-class b_tree
-{
+#include <iostream>
+
+class node {
 public:
-    node* root;
-    
-    b_tree() : root{nullptr} {}
-    
-    void _cleanup(node* n)
-    {
-        if (!n) return;
+  int key{};
+  int height{};
+  node *left{nullptr};
+  node *right{nullptr};
+  node *parent{nullptr};
 
-        _cleanup(n->left);
-        _cleanup(n->right);
+  node() = default;
+  explicit node(int d) : key{d} {}
+  node(node const &n) : key{n.key} {}
+  node(node &&n) noexcept = default;
+  node &operator=(node n) { return *this = std::move(n); }
 
-        n->left = n->right = n->parent = nullptr;
-        
-        delete n;
-    }
-    
-    void cleanup()
-    {
-        _cleanup(root);
-        
-        root = nullptr;
-    }
-    
-    ~b_tree()
-    {
-        cleanup();
-    }
+  bool operator>(node const &n) const { return key > n.key; }
+  bool operator==(node const &n) const = default;
+  ~node() = default;
 };
 
-node* leftmost_key(node* n)
-{
-    return n->left ? leftmost_key(n->left) : n;
+class b_tree {
+private:
+  void _cleanup(node *n) {
+    if (!n) return;
+
+    _cleanup(n->left);
+    _cleanup(n->right);
+
+    delete n;
+  }
+
+  void _cleanup() {
+    _cleanup(root);
+    root = nullptr;
+  }
+
+public:
+  node *root;
+
+  b_tree() : root{nullptr} {}
+  ~b_tree() { _cleanup(); }
+};
+
+node *leftmost_key(node *n) { return n->left ? leftmost_key(n->left) : n; }
+
+node *root_node(node *n) { return n->parent ? root_node(n->parent) : n; }
+
+bool is_left_alligned(node *n) { return n == n->parent->left; }
+
+node *climb(node *n) {
+  node *root{root_node(n)};
+  return *root > *n ? root : nullptr;
 }
 
-node* root_node(node* n)
-{
-    return n->parent ? root_node(n->parent) : n;
-}
+node *in_order_successor(node *n) {
+  if (!n) return nullptr;
 
-bool is_left_alligned(node* n)
-{
-    return n == n->parent->left;
-}
+  node *u = !n->right ? n : leftmost_key(n->right);
 
-node* climb(node* n)
-{
-    node* root{root_node(n)};
-    return *root > *n ? root : nullptr;
-}
-
-node* in_order_successor(node* n)
-{
-    if (!n) return nullptr;
-    
-    node* u = !n->right ? n : leftmost_key(n->right);
-    
-    if (u == n)
-    {
-        return is_left_alligned(n) ? n->parent : climb(n);
-    }
-    else return u;
+  if (u == n) {
+    return is_left_alligned(n) ? n->parent : climb(n);
+  }
+  return u;
 }
