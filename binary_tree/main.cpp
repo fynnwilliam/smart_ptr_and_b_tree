0fecca6da9b63e381a46a809e696d43810d8e8c6
fynnwilliam <fynnwilliam@gmail.com>
2022-06-07 09:47:19 -0400

    Implement void print_rows(node*)
    This function prints the nodes row by row.

--- binary_tree/main.cpp
+++ binary_tree/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 #include "b_tree.h"
 
@@ -11,11 +12,30 @@ void print(node *n) {
   std::cout << n->key << ' ';
 }
 
+void print_rows(node *n) {
+  std::queue<node*> q;
+  q.push(n);
+
+  while (!q.empty()) {
+    n = q.front();
+    std::cout << n->key << ' ';
+
+    if (n->left) q.push(n->left);
+    if (n->right) q.push(n->right);
+
+    q.pop();
+  }
+}
+
 int main() {
   b_tree tree{20};
 
   tree.build(tree.root(), 3);
   print(tree.root());
+
+  std::cout << '\n';
+
+  print_rows(tree.root());
 
   std::cout << '\n';
 }
